24b82b08c2123f17475968a768f33285b9629f8b
Matthew Calder <matt@Matthews-MacBook-Pro.local>
2014-01-09 00:57:34 -0800

    bug fixes

--- fetch_active.py
+++ fetch_active.py
@@ -8,15 +8,18 @@ URL = 'https://atlas.ripe.net/api/v1/probe/?limit=10000&format=txt'
 keys = ['id', 'asn_v4', 'asn_v6', 'prefix_v4', 'prefix_v6', 'status_name', 'country_code', 'latitude', 'longitude']
 
 def _str(s):
-    """
-    LOL
-    """
     if s == '' or s == ' ':
         return 'None'
     elif s == 'Never Connected':
         return 'NeverConnected'
     else:
         return str(s)
+
+def _int(i):
+    try:
+        return int(i)
+    except:
+        return None
 
 def json2tab(probe_list):
 
@@ -31,6 +34,33 @@ def usage_and_error():
     sys.stderr.write('Usage: <json|tab>\n')
     sys.exit(1)
 
+def loadtab(data):
+    types = [_int, _int, _int, str, str, str, str, float, float]
+    probe_list = []
+
+    for line in data.split('\n'):
+        try:
+            chunks = line.split(' ')
+            
+            """
+            types is a list of functions. the ith function gets
+            applied to the ith chunk using the lambda.
+            """
+            typed_chunks = map(lambda x,y:x(y), types, chunks)
+            """
+            This creates a dictionary by making the list, keys,
+            to be keys and its corresponding position in typed_chunks
+            to be the value.
+            """
+            probe_dict = dict(zip(keys, typed_chunks)) #nice!
+                    
+            probe_list.append(probe_dict)
+        except:
+            sys.stderr.write('Got error loading line: %s\n' % line)
+            continue
+        
+    return probe_list
+
 def load(file):
     """
     Returns a tuple with the file format and then a list of probe data
@@ -42,17 +72,7 @@ def load(file):
             return json.loads(data)
         except:
             #probably not json
-            probe_list = []
-            for line in data.split('\n'):
-                try:
-                    chunks = line.split(' ')
-                    probe_dict = dict(zip(keys, chunks)) #nice!
-                    probe_list.append(probe_dict)
-                except:
-                    sys.stderr.write('Got error loading line: %s\n' % line)
-                    traceback.print_exc(file=sys.stdout)
-                    continue
-            return probe_list
+            return loadtab(data)
     finally:
         f.close()
 
@@ -73,5 +93,3 @@ if __name__ == '__main__':
     else: #
         lines = json2tab(probe_list)
         print('\n'.join(lines))
-        
-    
