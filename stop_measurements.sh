980a9659c47b1180f16b93e3b4fae414066e2c1c
Matthew Calder <matt@Matthews-MacBook-Pro.local>
2014-02-08 08:00:15 -0800

    script to stop currently running measurements

---
+++ stop_measurements.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: measurement_id file"
+    exit 1
+fi
+
+key=$(cat ~/.atlas/stopkey)
+if [[ -z "$key" ]]; then
+    echo "Error: need to configure ~/.atlas/stopkey"
+    exit 1
+fi
+
+while read line; do
+    curl --dump-header - -X DELETE https://atlas.ripe.net/api/v1/measurement/$line/?key=$key
+done < $1
